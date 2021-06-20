ee77a81f48a1f01d0ef6299daec1484704f57ae3
Jeff Campbell <jeff@zapdot.com>
2021-06-19 21:06:53 -0400

    Created example ER plugin project and code generator

---
+++ ExamplePlugin/ExampleERCodeGeneratorPlugin.cs
@@ -0,0 +1,31 @@
+﻿using System.Linq;
+using EntitasRedux.Core.Plugins;
+using Genesis.Plugin;
+
+namespace ExamplePlugin
+{
+    public class ExampleERCodeGeneratorPlugin : ICodeGenerator
+    {
+        /// <inheritdoc />
+        public string Name => nameof(ExampleERCodeGeneratorPlugin);
+
+        /// <inheritdoc />
+        public int Priority => 0;
+
+        /// <inheritdoc />
+        public bool RunInDryMode => false;
+
+        /// <inheritdoc />
+        public CodeGenFile[] Generate(CodeGeneratorData[] data)
+        {
+            // Get all component data instances
+            var componentData = data.OfType<ComponentData>();
+
+            // Get all context data instances
+            var contextData = data.OfType<ContextData>();
+
+            // TODO Do something to generate additional
+            return new CodeGenFile[0];
+        }
+    }
+}
